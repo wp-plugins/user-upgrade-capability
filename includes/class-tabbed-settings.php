b8459ad160d9b642ce6cdfcb6e21238319ce2183
JustinTicktock <JustinTicktock@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-28 21:52:18 +0000

    1.2 initial commit
    git-svn-id: https://plugins.svn.wordpress.org/user-upgrade-capability/tags/1.2@998146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-tabbed-settings.php
@@ -0,0 +1,437 @@
+<?php
+/**
+ * Plugin tabbed settings option class for WordPress themes.
+ *
+ * @package   class-tabbed-settings.php
+ * @version   1.1.2
+ * @author    Justin Fletcher <justin@justinandco.com>
+ * @copyright Copyright (c) 2014, Justin Fletcher
+ * @license   http://opensource.org/licenses/gpl-2.0.php GPL v2 or later
+ *
+ * The text domain must be manually replaced with the plugin text domain.
+ */
+
+ 
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+
+/**
+ * Everything is pulled into this Class to allow for extendibility with including
+ * new callouts that are required on a plugin by plugin basis.
+ */
+if ( ! class_exists( 'Extendible_Tabbed_Settings' ) ) { 
+	Class Extendible_Tabbed_Settings  {
+
+		private $handlers = array();
+		
+		public function registerHandler($handler) {
+			$this->handlers[] = $handler;
+		}
+
+		public function __call($method, $arguments) {
+			foreach ($this->handlers as $handler) {
+				if (method_exists($handler, $method)) {
+					return call_user_func_array(
+						array($handler, $method),
+						$arguments
+					);
+				}
+			}
+		}
+		
+	}
+}
+
+if ( ! class_exists( 'Tabbed_Settings' ) ) {
+
+	/**
+	 * Tabbed_Settings class.
+	 */
+	class Tabbed_Settings extends Extendible_Tabbed_Settings {
+
+//		public static $instance;
+//		public $settings = array();
+//		public $config = array();
+
+
+		// the following are configurable externally
+        public $menu_access_capability = '';
+        public $menu_parent = '';
+        public $menu = '';
+        public $menu_title = '';
+        public $page_title = '';
+		public $default_tab_key = '';
+
+		/**
+		 * __construct function.
+		 *
+		 * @access public
+		 * @return void
+		 */	 
+		function __construct( $settings, $config ) {
+
+		//	$settings = $settings ;
+
+			$this->settings = $settings;
+//			$this->config = $config;
+			$this->register_config( $config );
+
+//			self::$instance = $this;
+
+			// hook priority = 9 to load settings before the class-tgm-plugin-activation.php runs with the same init hook at priority level 10
+			add_action( 'init', array( $this, 'init' ), 9 );
+			
+			add_action( 'admin_init', array( $this, 'render_setting_page' ) );
+
+			add_action( 'admin_menu', array( $this, 'add_admin_menus' ) );
+
+			add_action( 'do_settings_sections', array( $this, 'hooks_section_callback' ) );
+
+		}
+
+		/**
+		 * init function.
+		 *
+		 * @access private
+		 * @return void
+		 */
+		public function init() {
+		
+			do_action( 'tabbed_settings_register' );
+			// After this point, the settings should be registered and the configuration set.
+			
+		}
+
+		/**
+		 * Called during admin_menu, adds rendered using the plugin_options_page method.
+		 *
+		 * @access public
+		 * @return void
+		 */	 
+		public function add_admin_menus() {
+
+			add_submenu_page( $this->menu_parent, $this->page_title, $this->menu_title, $this->menu_access_capability, $this->menu, array( &$this, 'plugin_options_page' ));
+			
+		}
+
+
+        /**
+         * Add individual tabbed_settings to our collection of settings.
+         *
+         * If the required keys are not set or the tabbed_settings has already
+         * been registered, the plugin is not added.
+         *
+         * @since 2.0.0
+         *
+         * @param array $tabbed_settings Array of plugin arguments.
+         */
+        public function register_tabbed_settings( $settings ) {
+
+			foreach ( $settings as $tab_name => $registered_setting_page ) {
+				$this->settings[$tab_name] = $registered_setting_page;	
+//				self::$settings[$tab_name] = $registered_setting_page;	
+			}	
+        }
+
+        /**
+         * Amend default configuration.  This function strips out the config array elements and stores them 
+         * as a variable within the current Class object $this->"config-element" will be the means to return the 
+         * current configuration stored.
+         *
+         * @param array $config Array of config options to pass as class properties.
+		 * @return - sets up the CLASS object values $this->config.
+         */
+        public function register_config( $config ) {
+
+            $keys = array( 
+						'default_tab_key',
+						'menu_parent',
+						'menu_access_capability',
+						'menu',
+						'menu_title',
+						'page_title',
+					);
+
+            foreach ( $keys as $key ) {
+                if ( isset( $config[$key] ) ) {
+                    if ( is_array( $config[$key] ) ) {
+                        foreach ( $config[$key] as $subkey => $value ) {
+                           $this->{$key}[$subkey] = $value;
+                        }
+                    } else {
+                        $this->$key = $config[$key];
+                    }
+                }
+            }
+        }
+		
+		/**
+		 * render_setting_page function.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function render_setting_page(){
+
+			foreach ( $this->settings as $options_group => $section  ) {
+
+				if ( isset( $section['settings'] )) {
+					foreach ( $section['settings'] as $option ) {
+//						$this->current_section = $section;
+						
+						if ( isset( $option['std'] ) )
+							add_option( $option['name'], $option['std'] );
+						
+						$sanitize_callback = ( isset( $option['sanitize_callback'] ) ? $option['sanitize_callback'] : "" );
+//						$this->tabbed_settings[$options_group] = $section['title'];
+						register_setting( $options_group, $option['name'], $sanitize_callback );
+						add_settings_section( $options_group, $section['title'], array( $this, 'hooks_section_callback' ), $options_group );
+						
+						$callback_type = ( isset($option['type']) ? $option['type'] : "field_default_option" );
+						add_settings_field( $option['name'].'_setting-id', $option['label'], array( $this, $callback_type ), $options_group, $options_group, array( 'option' => $option )  );	
+					}
+				}
+			}
+		}
+		
+
+
+		/**
+		 * Settings page rendering it checks for active tab and replaces key with the related
+		 * settings key. Uses the plugin_options_tabs method to render the tabs.
+		 *
+		 * @access public
+		 * @return void
+		 */		 
+		public function plugin_options_page() {
+			
+			$tab = isset( $_GET['tab'] ) ? sanitize_key($_GET['tab'] ) : $this->default_tab_key;
+			if ( ! empty( $_GET['settings-updated'] )) {
+				do_action( 'tabbed_settings_after_update' );
+			}
+			?>
+			<div class="wrap">
+				<?php $this->plugin_options_tabs(); ?>
+				<form method="post" action="options.php">
+					<?php wp_nonce_field( 'update-options-nonce', 'update-options' ); ?>
+					<?php settings_fields( $tab ); ?>
+					<?php do_settings_sections( $tab ); ?>
+					<?php submit_button(); ?>
+				</form>
+			</div>
+			<?php
+		}
+
+		/**
+		 * Wordpress by default doesn't include a section callback to send any section descriptive text 
+		 * to the form.  This function uses section description based on the current section id being processed.
+		 *
+		 * @param section_passed.
+		 * @access public
+		 * @return void
+		 */	
+		public function hooks_section_callback( $section_passed ){
+			foreach ( $this->settings as $options_group => $section  ) {
+				if (( $section_passed['id'] == $options_group) && ( ! empty( $section['description'] ))) {	
+					echo esc_html( $this->settings[$options_group]['description'] );	
+				}
+			}
+		 }
+		
+		/**
+		 * field_checkbox_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_checkbox_option( array $args  ) {
+			$option   = $args['option'];
+			$value = get_option( $option['name'] );
+			?><label><input id="setting-<?php echo esc_html( $option['name'] ); ?>" name="<?php echo esc_html( $option['name'] ); ?>" type="checkbox" value="1" <?php checked( '1', $value ); ?> /> <?php echo esc_html( $option['cb_label'] ); ?></label><?php
+			if ( ! empty( $option['desc'] ))
+			echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';
+		}
+
+		/**
+		 * field_page_select_list_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_page_select_list_option( array $args  ) {
+			$option	= $args['option'];
+			$value	= intval( get_option( $option['name'] ));
+			?>
+			<form action="<?php site_url(); ?>" method="get">
+			<?php wp_dropdown_pages(array(
+										'show_option_none' => _x( '- None -', 'Nothing has been selected', 'user-upgrade-capability' ), 
+										'option_none_value' => '0', 
+										'sort_order'   => 'ASC',
+										'sort_column'  => 'post_title',
+										'hierarchical'  => 0,
+										'echo'          => 1,
+										'selected'     => $value,
+										'name'          => $option['name']
+										)); ?>
+			</form>
+
+			<?php
+			if ( ! empty( $option['desc'] ))
+				echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';		
+		}
+
+
+		/**
+		 * field_plugin_checkbox_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_plugin_checkbox_option( array $args  ) {
+		
+
+			$option   = $args['option'];
+			$filename = ( isset( $option['filename'] ) ? $option['filename'] : $option['slug'] );
+			$plugin_main_file =  trailingslashit( $option['plugin_dir']. $option['slug'] ) .  $filename . '.php' ;
+			$value = get_option( $option['name'] );
+
+			if ( is_plugin_active_for_network( $option['slug'] . '/' . $filename . '.php' )) {
+				?><label><input id="setting-<?php echo esc_html( $option['name'] ); ?>" name="<?php echo esc_html( $option['name'] ); ?>" type="checkbox" disabled="disabled" checked="checked"/> <?php
+			} else {
+				?><label><input id="setting-<?php echo esc_html( $option['name'] ); ?>" name="<?php echo esc_html( $option['name'] ); ?>" type="checkbox" value="1" <?php checked( '1', $value ); ?> /> <?php 
+			}
+
+			if ( ! file_exists( $plugin_main_file ) ) {
+				echo esc_html__( 'Enable to prompt installation and force active.', 'user-upgrade-capability' ) . ' ( ';
+				if ( $value ) echo '  <a href="' . add_query_arg( 'page', TGM_Plugin_Activation::$instance->menu, admin_url( 'themes.php' ) ) . '">' .  _x( 'Install', 'Install the Plugin', 'user-upgrade-capability' ) . " </a> | " ;
+				
+			} elseif ( is_plugin_active( $option['slug'] . '/' . $option['slug'] . '.php' ) &&  ! is_plugin_active_for_network( $option['slug'] . '/' . $option['slug'] . '.php' )) {
+				echo esc_html__(  'Force Active', 'user-upgrade-capability' ) . ' ( ';
+				if ( ! $value ) echo '<a href="plugins.php?s=' . esc_html( $option['label'] )	 . '">' .  _x( 'Deactivate', 'deactivate the plugin', 'user-upgrade-capability' ) . "</a> | " ;	
+			} else {
+				echo esc_html__(  'Force Active', 'user-upgrade-capability' ) . ' ( ';
+			}
+			echo ' <a href="http://wordpress.org/plugins/' . esc_html( $option['slug'] ) . '">' .  esc_html__( "wordpress.org", 'user-upgrade-capability' ) . " </a> )" ;		
+			?></label><?php
+			if ( ! empty( $option['desc'] ))
+				echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';
+		}	
+
+		/**
+		 * field_textarea_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_textarea_option( array $args  ) {
+
+			$option   = $args['option'];		
+			$defaults = array( 	
+						'columns' => "40",
+						'rows' => "3",
+						);
+				
+			$option = wp_parse_args( $option, $defaults );
+			$value = get_option( $option['name'] );
+			?><textarea id="setting-<?php echo esc_html( $option['name'] ); ?>" cols=<?php echo $option['columns']; ?> rows=<?php echo $option['rows']; ?> name="<?php echo esc_html( $option['name'] ); ?>" ><?php echo esc_textarea( $value ); ?></textarea><?php
+
+			if ( ! empty( $option['desc'] ))
+				echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';
+		}
+
+		/**
+		 * field_select_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_select_option( array $args  ) {
+			$option   = $args['option'];
+			$value = get_option( $option['name'] );
+			?><select id="setting-<?php echo esc_html( $option['name'] ); ?>" class="regular-text" name="<?php echo esc_html( $option['name'] ); ?>"><?php
+				foreach( $option['options'] as $key => $name )
+					echo '<option value="' . esc_attr( $key ) . '" ' . selected( $value, $key, false ) . '>' . esc_html( $name ) . '</option>';
+			?></select><?php
+
+			if ( ! empty( $option['desc'] ))
+				echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';
+		}
+		
+		/**
+		 * field_default_option 
+		 *
+		 * @param array of arguments to pass the option name to render the form field.
+		 * @access public
+		 * @return void
+		 */
+		public function field_default_option( array $args  ) {
+			$option   = $args['option'];
+			$value = get_option( $option['name'] );
+			?><input id="setting-<?php echo esc_html( $option['name'] ); ?>" class="regular-text" type="text" name="<?php echo esc_html( $option['name'] ); ?>" value="<?php esc_attr_e( $value ); ?>" /><?php
+
+			if ( ! empty( $option['desc'] ))
+				echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';
+		}
+		
+		/**
+		 * Renders our tabs in the plugin options page,
+		 * walks through the object's tabs array and prints
+		 * them one by one.
+		 *
+		 * @access public
+		 * @return void
+		 */
+		public function plugin_options_tabs() {
+		
+			$current_tab = isset( $_GET['tab'] ) ? sanitize_key( $_GET['tab'] ) : $this->default_tab_key;
+
+			screen_icon();
+			echo '<h2 class="nav-tab-wrapper">';
+			foreach ( $this->settings as $tab_key => $tab_options_array ) {
+				$active = $current_tab == $tab_key ? 'nav-tab-active' : '';
+				echo '<a class="nav-tab ' . $active . '" href="?page=' . $this->menu . '&tab=' . $tab_key . '">' . $tab_options_array['title'] . '</a>';	
+			}
+			echo '</h2>';
+		}
+
+
+		/**
+		 * selected_plugins function.
+		 *
+		 * @access public
+		 * @return array of plugins selected within the settings page for installation via the TGM_Plugin_Activation class
+		 */
+		public function selected_plugins( $plugin_extension_tab_name ) {
+
+			$plugins = array();
+
+			if ( isset( $this->settings ) ) {
+
+				$plugin_array = $this->settings[$plugin_extension_tab_name]['settings'];
+				
+				foreach ( $plugin_array as $plugin ) {
+
+					if ( get_option( $plugin['name'] ) ) {
+						// change the array element key name from 'label' to 'name' for use by TGM Activation
+						$plugin['option-name'] = $plugin['name'];
+						$plugin['name'] = $plugin['label'];
+						unset($plugin['label']);
+						$plugins[] = $plugin;
+					}
+				}
+
+			}
+			
+			return $plugins;
+		}
+		
+	}
+	
+}
+
+?>
---
+++ includes/settings.php
@@ -0,0 +1,243 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+// Append new links to the Plugin admin side
+add_filter( 'plugin_action_links_' . UUC::get_instance()->plugin_file , 'uuc_plugin_action_links');
+
+function uuc_plugin_action_links( $links ) {
+	$upgrade_user = UUC::get_instance();
+	
+	$settings_link = '<a href="users.php?page=' . $upgrade_user->menu . '">' . __( 'Settings' ) . "</a>";
+	array_push( $links, $settings_link );
+	return $links;	
+}
+
+
+// add action after the settings save hook.
+add_action( 'tabbed_settings_after_update', 'uuc_after_settings_update' );
+
+function uuc_after_settings_update( ) {
+
+	flush_rewrite_rules();	
+	
+}
+
+
+
+/**
+ * UUC_Settings class.
+ *
+ * Main Class which inits the CPTs and plugin
+ */
+class UUC_Settings {
+	
+	// Refers to a single instance of this class.
+    private static $instance = null;
+	
+	/**
+	 * __construct function.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	private function __construct() {
+	}
+
+	/**
+	 * Called during admin_menu, adds rendered using the plugin_options_page method.
+	 *
+	 * @access public
+	 * @return void
+	 */
+	public function add_admin_menus() {
+
+		add_submenu_page( 'users.php', __( 'Upgrade User Capability', 'user-upgrade-capability-text-domain' ), __( 'Upgrade Capability', 'user-upgrade-capability-text-domain' ), 'manage_network_users', UUC_Settings::get_instance()->menu, array( $this, 'plugin_options_page' ) );	
+	}
+	
+	/**
+     * Creates or returns an instance of this class.
+     *
+     * @return   A single instance of this class.
+     */
+    public static function get_instance() {
+
+		$upgrade_user = UUC::get_instance();
+		
+		$config = array(
+				'default_tab_key' => 'uuc_general',					// Default settings tab, opened on first settings page open.
+				'menu_parent' => 'users.php',    					// menu options page slug name.
+				'menu_access_capability' => 'manage_network_users',    					// menu options page slug name.
+				'menu' => $upgrade_user->menu,    					// menu options page slug name.
+				'menu_title' => $upgrade_user->menu_title,    		// menu options page slug name.
+				'page_title' => $upgrade_user->page_title,    		// menu options page title.
+				);
+
+		$settings = 	apply_filters( 'uuc_settings',
+			array(
+				'uuc_general' => array(
+					'title' 		=> __( 'General', 'user-upgrade-capability-text-domain' ),
+					'description' 	=> __( 'The "User Upgrade Capability" plugin simplifies the administration of user capabilities and allows several sites/blogs to have access rights cascaded out from one reference site.', 'user-upgrade-capability-text-domain' ),
+					'settings' 		=> array(		
+											array(
+												'name' 		=> 'uuc_reference_site',
+												'std' 		=> '0',
+												'label' 	=> __( 'Reference Site', 'user-upgrade-capability-text-domain' ),
+												'desc'		=> __( 'WARNING !! changing this setting will replace this site available roles/capabilities from the reference site selected YOU CANNOT UNDO.  Only change this option on new sites or when you wish to do so.  (If left as "-none-" this plugin will be disabled).', 'user-upgrade-capability-text-domain' ),
+												'type'      => 'field_site_list_option',
+												),
+											array(
+												'name' 		=> 'uuc_reference_key_capability',
+												'std' 		=> '',
+												'label' 	=> __( 'Reference Site Key Capability', 'user-upgrade-capability-text-domain' ),
+												'desc'		=> __( 'If left blank the site name will be used to search for as the capability on the reference site.', 'user-upgrade-capability-text-domain' ),
+												),													
+										),										
+				),
+				'uuc_user_capabilities' => array(
+					'title' 		=> __( 'User Capability', 'user-upgrade-capability-text-domain' ),
+					'description' 	=> __( 'Define here the capabilities that a user will receive for this site if they already have a single "key" capability on a reference site/blog within the multi-site network.  On user login to the current local site the plugin will expand the local capabilities to include those listed below. If the "key" reference capability is removed from the user at the reference site then all capabilities will be removed from this site the user will need to re-enrole.', 'user-upgrade-capability-text-domain' ),
+					'settings' 		=> array(														
+											array(
+												'columns'   => "45",
+												'rows'   	=> "8",
+												'name' 		=> 'uuc_additional_capabilties',
+												'std' 		=> '',
+												'label' 	=> __( 'Local site Additional Capabilities', 'user-upgrade-capability-text-domain' ),
+												'desc'		=> __( 'Put each capability on a separate row.', 'user-upgrade-capability-text-domain' ),
+												'type'      => 'field_textarea_option',
+												),
+										),										
+				),
+				'uuc_plugin_extension' => array(
+						'title' 		=> __( 'Plugin Extensions', 'user-upgrade-capability-text-domain' ),
+						'description' 	=> __( 'Most Plugins listed here are intended for use with the "Upgrade Use Capability" plugin.  Selection of a plugin will prompt you through the installation and the plugin will be forced network active while this is selected; deselecting will not remove the plugin, you will need to manually deactiate and uninstall from the network.', 'user-upgrade-capability-text-domain' ),					
+						'settings' 		=> array(
+												array(
+													'name' 		=> 'uuc_user_role_editor_plugin',
+													'std' 		=> false,
+													'label' 	=> 'User Role Editor',
+													'desc'		=> __( "This plugin gives the ability to edit users capabilities.  Once installed go to menu [users]..[User Role Editor].", 'user-upgrade-capability-text-domain' ),
+													'type'      => 'field_plugin_checkbox_option',
+													// the following are for tgmpa_register activation of the plugin
+													'slug'      			=> 'user-role-editor',
+													'plugin_dir'			=> UUC_PLUGIN_DIR,
+													'required'              => false,
+													'force_deactivation' 	=> false,
+													'force_activation'      => true,		
+													),							
+												array(
+													'name' 		=> 'uuc_join_my_multisite_plugin',
+													'std' 		=> false,
+													'label' 	=> 'Join My Multi-site',
+													'desc'		=> __( "This plugin allows for users to auto en-role on sites.  Once installed go to menu [users]..[Join My Multisite].", 'user-upgrade-capability-text-domain' ),
+													'type'      => 'field_plugin_checkbox_option',
+													// the following are for tgmpa_register activation of the plugin
+													'slug'      			=> 'join-my-multisite',
+													'plugin_dir'			=> UUC_PLUGIN_DIR,
+													'filename'      			=> 'joinmymultisite', 
+													'required'              => false,
+													'force_deactivation' 	=> false,
+													'force_activation'      => true,		
+													),
+												array(
+													'name' 		=> 'uuc_blog_copier_plugin',
+													'std' 		=> false,
+													'label' 	=> 'Blog Copier',
+													'desc'		=> __( "This is optional and is very helpful in the creation of new sites by simply copying an existing site, you can then create template sites for re-use.  Once installed go to the main Network..[sites]...[Blog Copier]", 'user-upgrade-capability-text-domain' ),
+													'type'      => 'field_plugin_checkbox_option',
+													// the following are for tgmpa_register activation of the plugin
+													'slug'      			=> 'blog-copier',
+													'plugin_dir'			=> UUC_PLUGIN_DIR,
+													'required'              => false,
+													'force_deactivation' 	=> false,
+													'force_activation'      => true,		
+													),
+												),
+					)
+				)
+			);
+		
+							
+        if ( null == self::$instance ) {
+            self::$instance = new Tabbed_Settings( $settings, $config );
+        }
+
+        return self::$instance;
+ 
+    } 
+}
+
+
+/**
+ * UUC_Settings_Additional_Methods class.
+ */
+
+/**
+ * UUC_Settings_Additional_Methods class.
+ */
+class UUC_Settings_Additional_Methods {
+
+	/**
+	 * field_site_list_option 
+	 *
+	 * @param array of arguments to pass the option name to render the form field.
+	 * @access public
+	 * @return void
+	 */
+	public function field_site_list_option( array $args   ) {
+
+		global $wpdb;
+
+		$defaults = array(
+					'name' 		=> '',
+					);
+			
+		$option = wp_parse_args( $args['option'], $defaults );
+		$value = get_option( $option['name'] );
+
+		$blogs = $wpdb->get_results("
+			SELECT blog_id
+			FROM {$wpdb->blogs}
+			WHERE site_id = '{$wpdb->siteid}'
+			AND spam = '0'
+			AND deleted = '0'
+			AND archived = '0'
+		");
+
+		$sites = array();
+		$sites[0] = '-None-';
+		foreach ($blogs as $blog) {
+			$sites[$blog->blog_id] = get_blog_option($blog->blog_id, 'blogname');
+		}
+		natsort($sites);
+
+		?>
+		<form action="<?php site_url(); ?>" method="get">
+			<select id="setting-<?php echo esc_html( $option['name'] ); ?>" class="regular-text" name="<?php echo esc_html( $option['name'] ); ?>">
+											<?php foreach( $sites as $site_id=>$site ) { ?>
+												<option value="<?php echo $site_id; ?>" <?php selected( $site_id, $value ); ?>><?php echo $site; ?></option>
+											<?php } ?>		
+			</select>
+				<class="description"> ( site_id:  <?php echo get_option('uuc_reference_site')?> )
+		</form>
+
+
+		<?php
+		
+		if ( ! empty( $option['desc'] ))
+			echo ' <p class="description">' . esc_html( $option['desc'] ) . '</p>';		
+	}
+}
+
+
+// Include the Tabbed_Settings class.
+
+require_once( dirname( __FILE__ ) . '/class-tabbed-settings.php' );
+
+// Create new tabbed settings object for this plugin..
+// and Include additional functions that are required.
+UUC_Settings::get_instance()->registerHandler( new UUC_Settings_Additional_Methods() );
+
+
+?>
